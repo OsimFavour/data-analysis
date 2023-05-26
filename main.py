55545cfc7f02a83b81efaa7fa34e3415707472ee
Osim Favour <famos204@gmail.com>
2023-05-26 04:51:20 +0100

    Modify main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-for i in range(1, 10):
-    print("fizzbuzz")
+import pandas as pd
+
+# READ CSV FILES
+df = pd.read_csv("data/survey_results_public.csv")
+schema_df = pd.read_csv("data/survey_results_schema.csv")
+
+# VIEW ALL ROWS AND COLUMNS TO ITS HIGHEST NUMBER
+pd.set_option("display.max_columns", 85)
+pd.set_option("display.max_rows", 85)
+
+df.head()
